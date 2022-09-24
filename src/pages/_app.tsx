54676b5093cbfda6b7f4698f53f185bde7c05b52
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-09-25 02:45:54 +0900

    feat:회원가입 및 로그인 기능
    context와 리듀서 사용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
+import Axios from "axios";
+import { AuthProvider } from "../context/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_URL + "/api";
+
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />;
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
